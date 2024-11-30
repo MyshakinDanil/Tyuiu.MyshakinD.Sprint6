0ad39f82a7377da9e4a98439d093ea1fa737cfee
Danil Myshakin <Tyuiu.MyshakinD@outlook.com>
2024-11-30 16:29:26 +0500

    Task0

---
+++ Tyuiu.MyshakinD.Sprint6.Task0.V6/FormMain.cs
@@ -0,0 +1,8 @@
+namespace Tyuiu.MyshakinD.Sprint6.Task0.V6 {
+    public partial class FormMain : Form {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
