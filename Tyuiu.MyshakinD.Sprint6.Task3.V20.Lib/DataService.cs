657d52d6668a35bcf4ebe7f92ce1b14589ef9b2d
Danil Myshakin <Tyuiu.MyshakinD@outlook.com>
2024-12-08 22:34:56 +0500

    Task3

---
+++ Tyuiu.MyshakinD.Sprint6.Task3.V20.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.MyshakinD.Sprint6.Task3.V20.Lib {
+    public class DataService : ISprint6Task3V20 {
+        public int[,] Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int columns = matrix.Length / rows;
+
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    if (i == 4 && matrix[i, j] % 2 == 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.MyshakinD.Sprint6.Task3.V20/FormMain.cs
@@ -0,0 +1,8 @@
+namespace Tyuiu.MyshakinD.Sprint6.Task3.V20 {
+    public partial class FormMain : Form {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
