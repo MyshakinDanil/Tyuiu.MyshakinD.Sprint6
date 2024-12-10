21ad83c8df6b5d46b7189fec80846830df58ebe6
Danil Myshakin <Tyuiu.MyshakinD@outlook.com>
2024-12-11 00:58:23 +0500

    Task7

--- Tyuiu.MyshakinD.Sprint6.Task7.V8/FormMain.Designer.cs
+++ Tyuiu.MyshakinD.Sprint6.Task7.V8/FormMain.Designer.cs
@@ -26,12 +26,238 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            panelControls = new Panel();
+            buttonHelp = new Button();
+            buttonSaveFile = new Button();
+            buttonGetResult = new Button();
+            buttonLoadFile = new Button();
+            panelTask = new Panel();
+            groupBoxTask = new GroupBox();
+            textBoxTask = new TextBox();
+            panelInput = new Panel();
+            dataGridViewInputData = new DataGridView();
+            splitter1 = new Splitter();
+            panelOutput = new Panel();
+            dataGridViewOutputData = new DataGridView();
+            openFileDialogTask = new OpenFileDialog();
+            toolTip1 = new ToolTip(components);
+            saveFileDialogMatrix = new OpenFileDialog();
+            panelControls.SuspendLayout();
+            panelTask.SuspendLayout();
+            groupBoxTask.SuspendLayout();
+            panelInput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewInputData).BeginInit();
+            panelOutput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewOutputData).BeginInit();
+            SuspendLayout();
+            // 
+            // panelControls
+            // 
+            panelControls.Controls.Add(buttonHelp);
+            panelControls.Controls.Add(buttonSaveFile);
+            panelControls.Controls.Add(buttonGetResult);
+            panelControls.Controls.Add(buttonLoadFile);
+            panelControls.Dock = DockStyle.Top;
+            panelControls.Location = new Point(0, 0);
+            panelControls.Name = "panelControls";
+            panelControls.Size = new Size(1118, 100);
+            panelControls.TabIndex = 0;
+            // 
+            // buttonHelp
+            // 
+            buttonHelp.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            buttonHelp.Cursor = Cursors.Hand;
+            buttonHelp.Image = (Image)resources.GetObject("buttonHelp.Image");
+            buttonHelp.Location = new Point(1012, 12);
+            buttonHelp.Name = "buttonHelp";
+            buttonHelp.Size = new Size(94, 82);
+            buttonHelp.TabIndex = 3;
+            toolTip1.SetToolTip(buttonHelp, "О программе");
+            buttonHelp.UseVisualStyleBackColor = true;
+            buttonHelp.Click += buttonHelp_Click;
+            // 
+            // buttonSaveFile
+            // 
+            buttonSaveFile.Cursor = Cursors.Hand;
+            buttonSaveFile.Enabled = false;
+            buttonSaveFile.Image = (Image)resources.GetObject("buttonSaveFile.Image");
+            buttonSaveFile.Location = new Point(212, 12);
+            buttonSaveFile.Name = "buttonSaveFile";
+            buttonSaveFile.Size = new Size(94, 82);
+            buttonSaveFile.TabIndex = 2;
+            toolTip1.SetToolTip(buttonSaveFile, "Сохранить данные в файл");
+            buttonSaveFile.UseVisualStyleBackColor = true;
+            buttonSaveFile.Click += buttonSaveFile_Click;
+            // 
+            // buttonGetResult
+            // 
+            buttonGetResult.Cursor = Cursors.Hand;
+            buttonGetResult.Enabled = false;
+            buttonGetResult.Image = (Image)resources.GetObject("buttonGetResult.Image");
+            buttonGetResult.Location = new Point(112, 12);
+            buttonGetResult.Name = "buttonGetResult";
+            buttonGetResult.Size = new Size(94, 82);
+            buttonGetResult.TabIndex = 1;
+            toolTip1.SetToolTip(buttonGetResult, "Выполнить операцию над данными из файла");
+            buttonGetResult.UseVisualStyleBackColor = true;
+            buttonGetResult.Click += buttonGetResult_Click;
+            // 
+            // buttonLoadFile
+            // 
+            buttonLoadFile.Cursor = Cursors.Hand;
+            buttonLoadFile.Image = (Image)resources.GetObject("buttonLoadFile.Image");
+            buttonLoadFile.Location = new Point(12, 12);
+            buttonLoadFile.Name = "buttonLoadFile";
+            buttonLoadFile.Size = new Size(94, 82);
+            buttonLoadFile.TabIndex = 0;
+            toolTip1.SetToolTip(buttonLoadFile, "Загрузить файл");
+            buttonLoadFile.UseVisualStyleBackColor = true;
+            buttonLoadFile.Click += buttonLoadFile_Click;
+            // 
+            // panelTask
+            // 
+            panelTask.Controls.Add(groupBoxTask);
+            panelTask.Dock = DockStyle.Top;
+            panelTask.Location = new Point(0, 100);
+            panelTask.Name = "panelTask";
+            panelTask.Size = new Size(1118, 119);
+            panelTask.TabIndex = 1;
+            // 
+            // groupBoxTask
+            // 
+            groupBoxTask.Controls.Add(textBoxTask);
+            groupBoxTask.Dock = DockStyle.Fill;
+            groupBoxTask.Location = new Point(0, 0);
+            groupBoxTask.Name = "groupBoxTask";
+            groupBoxTask.Size = new Size(1118, 119);
+            groupBoxTask.TabIndex = 1;
+            groupBoxTask.TabStop = false;
+            groupBoxTask.Text = "Условие";
+            // 
+            // textBoxTask
+            // 
+            textBoxTask.Dock = DockStyle.Fill;
+            textBoxTask.Font = new Font("Segoe UI", 13F);
+            textBoxTask.Location = new Point(3, 19);
+            textBoxTask.Multiline = true;
+            textBoxTask.Name = "textBoxTask";
+            textBoxTask.ReadOnly = true;
+            textBoxTask.Size = new Size(1112, 97);
+            textBoxTask.TabIndex = 0;
+            textBoxTask.Text = resources.GetString("textBoxTask.Text");
+            // 
+            // panelInput
+            // 
+            panelInput.Controls.Add(dataGridViewInputData);
+            panelInput.Dock = DockStyle.Left;
+            panelInput.Location = new Point(0, 219);
+            panelInput.Name = "panelInput";
+            panelInput.Size = new Size(546, 422);
+            panelInput.TabIndex = 2;
+            // 
+            // dataGridViewInputData
+            // 
+            dataGridViewInputData.AllowUserToAddRows = false;
+            dataGridViewInputData.AllowUserToDeleteRows = false;
+            dataGridViewInputData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewInputData.ColumnHeadersVisible = false;
+            dataGridViewInputData.Dock = DockStyle.Fill;
+            dataGridViewInputData.Location = new Point(0, 0);
+            dataGridViewInputData.Name = "dataGridViewInputData";
+            dataGridViewInputData.ReadOnly = true;
+            dataGridViewInputData.RowHeadersVisible = false;
+            dataGridViewInputData.Size = new Size(546, 422);
+            dataGridViewInputData.TabIndex = 0;
+            // 
+            // splitter1
+            // 
+            splitter1.Location = new Point(546, 219);
+            splitter1.Name = "splitter1";
+            splitter1.Size = new Size(3, 422);
+            splitter1.TabIndex = 3;
+            splitter1.TabStop = false;
+            // 
+            // panelOutput
+            // 
+            panelOutput.Controls.Add(dataGridViewOutputData);
+            panelOutput.Dock = DockStyle.Fill;
+            panelOutput.Location = new Point(549, 219);
+            panelOutput.Name = "panelOutput";
+            panelOutput.Size = new Size(569, 422);
+            panelOutput.TabIndex = 4;
+            // 
+            // dataGridViewOutputData
+            // 
+            dataGridViewOutputData.AllowUserToAddRows = false;
+            dataGridViewOutputData.AllowUserToDeleteRows = false;
+            dataGridViewOutputData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewOutputData.ColumnHeadersVisible = false;
+            dataGridViewOutputData.Dock = DockStyle.Fill;
+            dataGridViewOutputData.Location = new Point(0, 0);
+            dataGridViewOutputData.Name = "dataGridViewOutputData";
+            dataGridViewOutputData.ReadOnly = true;
+            dataGridViewOutputData.RowHeadersVisible = false;
+            dataGridViewOutputData.Size = new Size(569, 422);
+            dataGridViewOutputData.TabIndex = 0;
+            // 
+            // openFileDialogTask
+            // 
+            openFileDialogTask.FileName = "openFileDialogTask";
+            // 
+            // toolTip1
+            // 
+            toolTip1.ToolTipIcon = ToolTipIcon.Info;
+            toolTip1.ToolTipTitle = "Подсказка";
+            // 
+            // saveFileDialogMatrix
+            // 
+            saveFileDialogMatrix.FileName = "saveFileDialogMatrix";
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1118, 641);
+            Controls.Add(panelOutput);
+            Controls.Add(splitter1);
+            Controls.Add(panelInput);
+            Controls.Add(panelTask);
+            Controls.Add(panelControls);
+            MinimumSize = new Size(1134, 680);
+            Name = "FormMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Спринт #6 | Выполнил: Мышакин Д. | ПКТб-24-1";
+            WindowState = FormWindowState.Maximized;
+            panelControls.ResumeLayout(false);
+            panelTask.ResumeLayout(false);
+            groupBoxTask.ResumeLayout(false);
+            groupBoxTask.PerformLayout();
+            panelInput.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewInputData).EndInit();
+            panelOutput.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewOutputData).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelControls;
+        private Button buttonHelp;
+        private Button buttonSaveFile;
+        private Button buttonGetResult;
+        private Button buttonLoadFile;
+        private Panel panelTask;
+        private Panel panelInput;
+        private Splitter splitter1;
+        private Panel panelOutput;
+        private GroupBox groupBoxTask;
+        private TextBox textBoxTask;
+        private DataGridView dataGridViewInputData;
+        private DataGridView dataGridViewOutputData;
+        private OpenFileDialog openFileDialogTask;
+        private ToolTip toolTip1;
+        private OpenFileDialog saveFileDialogMatrix;
     }
 }
--- Tyuiu.MyshakinD.Sprint6.Task7.V8/FormMain.cs
+++ Tyuiu.MyshakinD.Sprint6.Task7.V8/FormMain.cs
@@ -1,8 +1,126 @@
+using System.IO;
+using Tyuiu.MyshakinD.Sprint6.Task6.V5;
+using Tyuiu.MyshakinD.Sprint6.Task7.V8.Lib;
+
 namespace Tyuiu.MyshakinD.Sprint6.Task7.V8 {
     public partial class FormMain : Form {
         public FormMain()
         {
             InitializeComponent();
+
+            openFileDialogTask.Filter = "Значения, разделённые запятыми(*.csv)|*.csv|Все файлы(*.*)|*.*";
+            saveFileDialogMatrix.Filter = "Значения, разделённые запятыми(*.csv)|*.csv|Все файлы(*.*)|*.*";
+        }
+
+        int rows;
+        int columns;
+        string openFilePath;
+
+        DataService ds = new DataService();
+
+        private void buttonLoadFile_Click(object sender, EventArgs e)
+        {
+            openFileDialogTask.ShowDialog();
+            openFilePath = openFileDialogTask.FileName;
+
+            string filedata = File.ReadAllText(openFilePath);
+
+            filedata = filedata.Replace('\n', '\r');
+            string[] lines = filedata.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            rows = lines.Length;
+            columns = lines[0].Split(';').Length;
+
+            int[,] arrayValues = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] line_r = lines[i].Split(';');
+                for (int j = 0; j < columns; j++)
+                {
+                    arrayValues[i, j] = Convert.ToInt32(line_r[j]);
+                }
+            }
+
+            dataGridViewInputData.ColumnCount = columns;
+            dataGridViewInputData.RowCount = rows;
+            dataGridViewOutputData.ColumnCount = columns;
+            dataGridViewOutputData.RowCount = rows;
+
+            for (int i = 0; i < columns; i++)
+            {
+                dataGridViewInputData.Columns[i].Width = 25;
+                dataGridViewOutputData.Columns[i].Width = 25;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    dataGridViewInputData.Rows[i].Cells[j].Value = arrayValues[i, j];
+                }
+            }
+
+            buttonGetResult.Enabled = true;
+        }
+
+        private void buttonGetResult_Click(object sender, EventArgs e)
+        {
+            int[,] arrayValues = new int[rows, columns];
+            arrayValues = ds.GetMatrix(openFilePath);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    dataGridViewOutputData.Rows[i].Cells[j].Value = arrayValues[i, j];
+                }
+            }
+
+            buttonSaveFile.Enabled = true;
+        }
+
+        private void buttonSaveFile_Click(object sender, EventArgs e)
+        {
+            saveFileDialogMatrix.FileName = "OutPutFileTask7.csv";
+            saveFileDialogMatrix.InitialDirectory = Directory.GetCurrentDirectory();
+            saveFileDialogMatrix.ShowDialog();
+
+            string path = saveFileDialogMatrix.FileName;
+
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+
+            string str = "";
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j != columns - 1)
+                    {
+                        str = str + dataGridViewOutputData.Rows[i].Cells[j].Value + ";";
+                    }
+                    else
+                    {
+                        str = str + dataGridViewOutputData.Rows[i].Cells[j].Value;
+                    }
+                }
+
+                File.AppendAllText(path, str + Environment.NewLine);
+                str = "";
+            }
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            FormAbout formAbout = new FormAbout();
+            formAbout.ShowDialog();
         }
     }
 }
